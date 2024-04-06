0235eaebc68a6fb68a873ac1a6c6419bd2a68b3e
teresstinga <tpearl2594@gmail.com>
2024-04-05 23:58:43 -0600

    lab 5

---
+++ components/AboutScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const AboutScreen = ({ } ) => {
+    // ...
+
+    return (
+        <>
+            <Text>Pearl's Boring To Do List App</Text>
+        </>
+    );
+}
+
+export default HomeScreen;
